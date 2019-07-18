4d0dbb4a9555e70dcf8e99b531b9b6ec956b58e1
EC2 Default User <ec2-user@ip-172-31-40-26.us-east-2.compute.internal>
2019-07-18 17:59:50 +0000

    Update 4

--- calc.py
+++ calc.py
@@ -2,20 +2,23 @@ num1=input("what is the first number? \n")
 op = input("What operation will you be using? \n")
 num2=input ("What is the second number? \n")
 
-
-if op == "+" or op == "add" or op == "addition" or op =="plus":
+total = 0
+if op.lower() == "+" or op.lower() == "add" or op.lower() == "addition" or op.lower() =="plus":
     total = int(num1) + int(num2)
     sym = "+"
-elif op == "-" or op == "subtract" or op == "minus":
+    
+elif op.lower() == "-" or op.lower() == "subtract" or op.lower() == "minus":
      total = int(num1) - int(num2)
      sym = "-"
-elif op == "*" or op == "multiply" or op == "multiplication":
+     
+elif op.lower() == "*" or op.lower() == "multiply" or op.lower() == "multiplication":
     total= int(num1) * int(num2)
     sym = "*"
-elif op == "/" or op == "divide" or op == "division":
+    
+elif op.lower() == "/" or op.lower() == "divide" or op.lower() == "division":
     total= int(num1) / int(num2)
     sym = "/"
-
-
-print(str(num1) + " " + sym + " "+ str(num2) +  " = "  + str(total))
+    
+    
+print(str(num1) + " " + str(sym) + " " + str(num2) +  " = "  + str(total))
 
