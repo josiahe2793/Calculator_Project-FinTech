4eb695f3b9750199a9976258a5e39252a6ab8bff
EC2 Default User <ec2-user@ip-172-31-40-26.us-east-2.compute.internal>
2019-07-18 17:09:26 +0000

    Minimum Viable Product (MVP)

--- calc.py
+++ calc.py
@@ -1,1 +1,17 @@
-print("My calculator")
+num1=input("what is the first number? ")
+op = input("What operation will you be using? ")
+num2=input ("What is the second number? ")
+
+
+if op == "+":
+    total = int(num1) + int(num2)
+elif op == "-":
+     total = int(num1) - int(num2)
+elif op == "*":
+    total= int(num1) * int(num2)
+elif op == "/":
+    total= int(num1) / int(num2)
+
+
+print(str(total))
+
