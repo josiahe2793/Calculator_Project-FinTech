a4cd2880b5d2d2791955122c397cf0c6384158f5
EC2 Default User <ec2-user@ip-172-31-40-26.us-east-2.compute.internal>
2019-07-18 17:22:12 +0000

    MVP update

--- calc.py
+++ calc.py
@@ -3,15 +3,19 @@ op = input("What operation will you be using? ")
 num2=input ("What is the second number? ")
 
 
-if op == "+":
+if op == "+" or "add" or "addition" or "plus":
     total = int(num1) + int(num2)
-elif op == "-":
+    sym = "+"
+elif op == "-" or "subtract" or "minus":
      total = int(num1) - int(num2)
-elif op == "*":
+     sym = "-"
+elif op == "*" or "multiply" or "multiplication":
     total= int(num1) * int(num2)
-elif op == "/":
+    sym = "*"
+elif op == "/" or "divide" or "division":
     total= int(num1) / int(num2)
+    sym = "/"
 
 
-print(str(total))
+print(str(num1) + " " + sym + " "+ str(num2) +  " = "  + str(total))
 
